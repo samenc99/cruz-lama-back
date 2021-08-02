61db7eeaf641a20e905f371646e99c65a21141f8
samenc99 <mateus.enc@gmail.com>
2021-08-02 14:45:37 -0300

    gitignore

--- build/src/services/IdGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.idGenerator = void 0;
-const uuid_1 = require("uuid");
-const idGenerator = () => {
-    return uuid_1.v4();
-};
-exports.idGenerator = idGenerator;
-//# sourceMappingURL=IdGenerator.js.map
--- build/tests/CustomDate.test.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=CustomDate.test.js.map
